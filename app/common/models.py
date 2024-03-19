feb574ac2a2d745820c2370ae25fa9dd71d7422d
KangJeongHo1 <rkdwjdgh04@naver.com>
2024-03-19 11:13:17 +0900

    create common model

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 class CommonModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True) # 생성된 시간
+
+    # 데이터가 업데이트 된 시간
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # DB에 테이블을 추가하지 마세요. 
